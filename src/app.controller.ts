4c33fca59160673872801dcabc795603b46dbd8b
joooonis <xowns3213@naver.com>
2024-05-12 12:43:02 +0900

    ✨ feat : controllers

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller() // express의 라우터 역할
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get() // 데코레이터랑 함수는 붙여서 작성해야함
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/hello') // url을 등록하고 함수를 매핑
+  sayHello(): string {
+    return 'Hello everyone!';
   }
 }
