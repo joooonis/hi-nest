493053e150fa16aeeac0573fe830afa1ea6dace7
joooonis <xowns3213@naver.com>
2024-05-15 15:17:21 +0900

    ✨ feat : movies controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
 
 // 데코레이터 : 클래스를 위한 함수
 @Module({
   imports: [],
-  controllers: [AppController], // url을 가져오고 함수를 실행하는 역할
-  providers: [AppService],
+  controllers: [MoviesController], // url을 가져오고 함수를 실행하는 역할
+  providers: [],
 })
 export class AppModule {}
