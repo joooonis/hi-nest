397e8f3cbeb2f9e2cbad43f3dbba3145d6413963
joooonis <xowns3213@naver.com>
2024-05-15 15:36:49 +0900

    ✨ feat : routes with body and query decorator

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies') // url의 엔트리 포인트
 export class MoviesController {
@@ -7,14 +16,21 @@ export class MoviesController {
     return 'This will return all movies';
   }
 
+  @Get('search') // :id와 search가 겹치면 id로 인식하기 때문에 search를 위로 올려준다.
+  search(@Query('year') searchingYear: string) {
+    return `This will return movies after the year: ${searchingYear}`;
+  }
+
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `This will return a movie with the id: ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'This will create a movie';
+  create(@Body() movieData) {
+    return {
+      createdMovie: movieData,
+    };
   }
 
   @Delete('/:id')
@@ -23,7 +39,10 @@ export class MoviesController {
   }
 
   @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id: ${movieId}`;
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatetedMovie: movieId,
+      ...updateData,
+    };
   }
 }
