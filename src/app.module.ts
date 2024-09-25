d6ba0ff0b2df6403929c5526e3a2e4e54b79fc4e
ed <ed@bvrly.ai>
2024-09-25 23:05:01 +0900

    feat: add movies service and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MoivesService } from './moives/moives.service';
 import { MoviesController } from './movies/movies.controller';
 
 // 데코레이터 : 클래스를 위한 함수
 @Module({
   imports: [],
   controllers: [MoviesController], // url을 가져오고 함수를 실행하는 역할
-  providers: [],
+  providers: [MoivesService],
 })
 export class AppModule {}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,12 +8,16 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { Movie } from 'src/moives/entities/movie.entity';
+import { MoivesService } from 'src/moives/moives.service';
 
 @Controller('movies') // url의 엔트리 포인트
 export class MoviesController {
+  constructor(private readonly moviesService: MoivesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Get('search') // :id와 search가 겹치면 id로 인식하기 때문에 search를 위로 올려준다.
@@ -21,21 +25,19 @@ export class MoviesController {
     return `This will return movies after the year: ${searchingYear}`;
   }
 
-  @Get('/:id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return a movie with the id: ${movieId}`;
+  @Get(':id')
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
-  create(@Body() movieData) {
-    return {
-      createdMovie: movieData,
-    };
+  create(@Body() movieData: Movie) {
+    return this.moviesService.create(movieData);
   }
 
   @Delete('/:id')
   remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch('/:id')
